f6bc16c048e3e3d11bd78108f03ad7cf3466c29c
codebyMoh <codebymohit@gmail.com>
2025-09-01 09:55:18 +0530

    add hardik code

--- app/(tabs)/share_qr/index.tsx
+++ app/(tabs)/share_qr/index.tsx
@@ -1,5 +1,6 @@
 import { AntDesign } from "@expo/vector-icons";
-import * as Clipboard from "expo-clipboard";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Octicons from "@expo/vector-icons/Octicons";
 import { Stack, router } from "expo-router";
 import {
   SafeAreaView,
@@ -14,36 +15,55 @@ export default function ShareQRCodeScreen() {
   const walletAddress = "0xE947D41FC4459818f8697AdAdf0e5C4606BB5f73";
   const userName = "meetdesai10";
 
-  const copyToClipboard = async () => {
-    await Clipboard.setStringAsync(walletAddress);
-    console.log("Wallet address copied to clipboard!");
-  };
-
   return (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{ headerShown: false }} />
       {/* Header */}
-      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
-        <AntDesign name="arrowleft" size={28} color="black" />
+      <TouchableOpacity style={styles.backButton}>
+        <AntDesign
+          name="arrowleft"
+          size={28}
+          color="black"
+          onPress={() => router.back()}
+        />
+        <Octicons name="download" size={24} color="black" />
       </TouchableOpacity>
 
-      <View style={styles.content}>
-        <Text style={styles.title}>Scan to Pay {userName}</Text>
-        <Text style={styles.subtitle}>Show this QR code to receive money</Text>
+      <View style={styles.card}>
+        <Text style={styles.userName}>{userName}</Text>
 
-        <View style={styles.qrCodeContainer}>
-          {/* QR Code Generator */}
+        <View style={styles.qrContainer}>
           <QRCodeSvg
             value={walletAddress}
-            size={200}
+            size={220}
             color="black"
             backgroundColor="white"
+            logo={require("../../../assets/images/logos/Zink-Logo.png")}
+            logoSize={80}
+            logoMargin={0}
+            logoBackgroundColor="white"
+            logoBorderRadius={40}
           />
         </View>
 
-        <Text style={styles.walletAddressText}>{walletAddress}</Text>
-        <TouchableOpacity style={styles.copyButton} onPress={copyToClipboard}>
-          <Text style={styles.copyButtonText}>Copy Wallet Address</Text>
+        <Text style={styles.walletText}>Id: 97AdAdf0e5C4606BB5f7</Text>
+      </View>
+      <View style={styles.bottomButtons}>
+        <TouchableOpacity
+          style={styles.scannerButton}
+          onPress={() => router.push("/scan_qr")}
+        >
+          <MaterialCommunityIcons name="qrcode-scan" size={24} color="black" />
+          <Text
+            style={styles.scannerButtonText}
+            onPress={() => router.push("/scan_qr")}
+          >
+            Open scanner
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.shareButton}>
+          <MaterialCommunityIcons name="share-variant" size={24} color="#fff" />
+          <Text style={styles.shareButtonText}>Share QR code</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -55,72 +75,86 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     padding: 20,
+    justifyContent: "space-between",
   },
   backButton: {
     padding: 10,
     marginTop: 20,
-  },
-  header: {
-    width: "100%",
-    alignItems: "flex-start",
-    marginTop: 20,
-  },
-  closeButton: {
-    padding: 5,
-  },
-  content: {
-    flex: 1,
+    display: "flex",
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    paddingBottom: 20,
+    justifyContent: "space-between",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#000",
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: "#555",
-    marginBottom: 30,
-    textAlign: "center",
-  },
-  qrCodeContainer: {
-    backgroundColor: "#fff",
+  card: {
+    backgroundColor: "#f9f9f9",
+    borderRadius: 16,
+    alignItems: "center",
     padding: 20,
-    borderRadius: 15,
     shadowColor: "#000",
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    elevation: 8,
-    marginBottom: 30,
-  },
-  walletAddressText: {
-    fontSize: 12,
-    color: "#888",
-    textAlign: "center",
-    marginBottom: 15,
-    paddingHorizontal: 20,
-  },
-  copyButton: {
-    backgroundColor: "#f0f0f0",
-    borderRadius: 15,
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    alignItems: "center",
-    width: "80%",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.05,
-    shadowRadius: 3,
+    shadowOffset: { width: 0, height: 3 },
+    shadowRadius: 5,
     elevation: 3,
   },
-  copyButtonText: {
+  userName: {
+    fontSize: 18,
+    fontWeight: "bold",
     color: "#000",
-    fontSize: 16,
+    marginBottom: 20,
+  },
+  qrContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 12,
+    marginBottom: 20,
+  },
+  walletText: {
+    fontSize: 12,
+    color: "#555",
+    textAlign: "center",
+  },
+  bottomButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  shareButton: {
+    width: "45%",
+    display: "flex",
+    flexDirection: "row",
+    gap: 3,
+    justifyContent: "center",
+    backgroundColor: "#5abb5eff",
+    borderRadius: 30,
+    padding: 12,
+    marginBottom: 8,
+    marginHorizontal: 5,
+    paddingVertical: 15,
+    alignItems: "center",
+  },
+  shareButtonText: {
+    fontSize: 14,
     fontWeight: "600",
+    color: "#fff",
+  },
+  scannerButton: {
+    width: "45%",
+    display: "flex",
+    flexDirection: "row",
+    gap: 5,
+    justifyContent: "center",
+    borderColor: "#f0f0f0",
+    borderWidth: 3,
+    borderRadius: 30,
+    padding: 12,
+    marginBottom: 8,
+    marginHorizontal: 5,
+    paddingVertical: 15,
+    alignItems: "center",
+  },
+  scannerButtonText: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#000",
   },
 });
